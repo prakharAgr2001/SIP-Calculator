e37f1877ce6678d4aa6ac37fe3397f1b95169d81
Prakhar Agrawal <f20202110@goa.bits-pilani.ac.in>
2024-10-16 00:11:25 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react';
-import './App.css'; // Import the CSS for crazy design
+import './App.css'; 
 
 function App() {
   const [monthlyInvestment, setMonthlyInvestment] = useState('');
